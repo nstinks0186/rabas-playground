35984875a35d0c30bffd2feb8bf1f1f28d9fb7b5
Pinuno Fuentes <nstinks0186@gmail.com>
2015-10-13 19:24:13 -0400

    Update TourPackage

--- Tourista/TourPackage.swift
+++ Tourista/TourPackage.swift
@@ -9,8 +9,28 @@
 import Foundation
 
 class TourPackage {
-    var name: String = ""
-    var destination: String = ""
+    var name: String
+    var provider: TourProvider
+    
+    var destinationList: [TourDestination]?
+    var startDestination: TourDestination?
+    var endDestination: TourDestination?
+    
+    var duration:Int = 1 // number of days (for 1 day or less)
+    
+    var miscData: [String: AnyObject]?
+    
+    var sourceURL: String?
+    
+    init(name:String, provider:String){
+        self.name = name
+        self.provider = TourProvider(name: provider)
+    }
+    
+    convenience init(name:String, provider:String, duration:Int){
+        self.init(name:name, provider:provider)
+        self.duration = duration
+    }
 }
 
 func mocklistTourPackage() -> [TourPackage] {
